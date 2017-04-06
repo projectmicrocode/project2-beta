6f1eaa58b52c6aa7d75a43251487675ec67e3897
Hung Anh <project.microcode@gmail.com>
2017-04-06 16:33:08 +0700

    Chinh danhsachdetaichoduyet.blade.php

--- resources/views/layouts/giangvienphutrach/danhsachdetaichoduyet.blade.php
+++ resources/views/layouts/giangvienphutrach/danhsachdetaichoduyet.blade.php
@@ -8,131 +8,131 @@
 
 
 <div class="col-lg-8">
-		<ul class="nav nav-tabs">
-  			<li role="presentation" class="active"><a href="{{url('chitietdetaichoduyet')}}">Danh sách đề tài chờ duyệt</a></li>
+	<ul class="nav nav-tabs">
+		<li role="presentation" class="active"><a href="{{url('chitietdetaichoduyet')}}">Danh sách đề tài chờ duyệt</a></li>
 
-  			<li role="presentation" "><a href="{{url('danhsachsinhvienchoduyet')}}">Danh sách sinh viên chờ duyệt</a></li>
-  			<li role="presentation"><a href="{{url('danhsachsinhviendacbietchoduyet')}}">Danh sách sinh viên đặc biệt</a></li>
-  			
-		</ul>
-		<br>
-		<br>
-		<div class="content-list" >
+		<li role="presentation" "><a href="{{url('danhsachsinhvienchoduyet')}}">Danh sách sinh viên chờ duyệt</a></li>
+		<li role="presentation"><a href="{{url('danhsachsinhviendacbietchoduyet')}}">Danh sách sinh viên đặc biệt</a></li>
+
+	</ul>
+	<br>
+	<br>
+	<div class="content-list" >
 		@foreach ($data as $retrieve)
-        @if($retrieve->tinhtrang == 0)
-      
-            <ul>
+		@if($retrieve->tinhtrang == 0)
+
+		<ul>
 			<li>
 				<h4>
-					<span>Tên Đề Tài: </span><b><a href="#">{{$retrieve->tendetai}}</a></b>
+					<span>Tên đề tài: </span><b><a href="#">{{$retrieve->tendetai}}</a></b>
 					
 
 				</h4>
 			</li>
 			<li>
-			<span>Số Sinh Viên Nhận: </span>{{$retrieve->soluong}}
-		
+				<span>Số sinh viên nhận: </span>{{$retrieve->soluong}}
+
 			</li><br>
 			<li>
-			<span>Công Ty: </span>{{$retrieve->name}}
-		
+				<span>Công ty: </span>{{$retrieve->name}}
+
 			</li><br>
-			<li><span>Thời Gian Tạo: </span>{{$retrieve->created_at}}
+			<li><span>Thời gian tạo: </span>{{$retrieve->created_at}}
 			</li>
 			<br>
 			<div class="col-xs-1">
 				<form action="duyetDeTai/<?php echo $retrieve->id?>" method="POST">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<button class="btn btn-primary" href="#" id="btnDuyet">Duyệt</button>
-				
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					<button class="btn btn-primary" href="#" id="btnDuyet">Duyệt</button>
+
 				</form>
 			</div>
 			
-            <div class="col-xs-2">
-            	 <form action="chitietdetaichoduyet/<?php echo $retrieve->id?>" method="POST">
+			<div class="col-xs-2">
+				<form action="chitietdetaichoduyet/<?php echo $retrieve->id?>" method="POST">
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 					<button name="btnChiTietDeTai" class="btn btn-primary"> Chi Tiết</button>
 					
-			</form>
-            </div>
-           
-			</ul>
-			<br>
-			<br>
+				</form>
+			</div>
 
-          
-        @endif
-        @endforeach
+		</ul>
+		<br>
+		<br>
 
-			
-		</div>
-		
 
-   		
+		@endif
+		@endforeach
+
+
 	</div>
-		<div class="col-lg-4">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h3 class="panel-title">Danh sách đề tài đã được duyệt</h3>
-				</div>
-				<div class="panel-body">
-						
-					@foreach ($data as $retrieve)
-						@if($retrieve->tinhtrang == 1)
-							<ul class="list-group">
-								<li class="list-group-item"><span></span>{{$retrieve->tendetai}}</li>
-								<li class="list-group-item"><span></span>{{$retrieve->name}}</li>
-							</ul>
-						@endif
-					@endforeach
-						
-				</div>
-
-			</div>
-			<br>
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h3 class="panel-title">Deadline Công Ty Nộp Đề Tài</h3>
-				</div>
-				<div class="panel-body">
-					<form name  ="frmSinhVien" class="form-horizontal" action="datdeadline" method="POST">
-						<input type ="hidden" name="_token" value="{{ csrf_token() }}">
-						<div class="content-panel">
-							<div class  ="form-group">
-								<label for  ="inputEmail3" class="col-sm-4 control-label">Thời Gian</label>
-								<div class  ="col-sm-7">
-									<input type ="text" class="form-control" id="txtdeadline" name="txtdeadline" placeholder="DD-MM-YYYY H:M:S" />
-								</div >
-								
-								<div style="padding-left: 40px" class  ="col-sm-12"><span id="helpBlock2" class="help-block" >Vui lòng nhập theo mẫu DD-MM/-YYY H:M:S </span></div>
-								</div>
-								
-								@if (session('status'))
-								<div class="alert alert-success">
-								{{ session('status') }}
-								</div>
-								@endif
-								
-								
-								
-								
-								
-								<div class="modal-footer">
-								
-								<input type ="submit" name="submit" id="dangkidetai" class="btn btn-primary btn-lg btn-block" value="Đặt">
-								
-								</div>     
-					</form>
-				</div>
-			</div>
-			s
-
-
-
 
 </div>
+<div class="col-lg-4">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Danh sách đề tài đã được duyệt</h3>
+		</div>
+		<div class="panel-body">
+
+			@foreach ($data as $retrieve)
+			@if($retrieve->tinhtrang == 1)
+			<ul class="list-group" >
+				<li class="list-group-item" style="background: #f8f8f8; color: #337ab7"><span class="glyphicon glyphicon-ok"></span>		{{$retrieve->tendetai}}</li>
+				<li class="list-group-item"><span></span>{{$retrieve->name}}</li>
+			</ul>
+			@endif
+			@endforeach
+
+		</div>
+
+	</div>
+	<br>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Deadline Công Ty Nộp Đề Tài</h3>
+		</div>
+		<div class="panel-body">
+			<form name  ="frmSinhVien" class="form-horizontal" action="datdeadline" method="POST">
+				<input type ="hidden" name="_token" value="{{ csrf_token() }}">
+				<div class="content-panel">
+					<div class  ="form-group">
+						<label for  ="inputEmail3" class="col-sm-4 control-label">Thời Gian</label>
+						<div class  ="col-sm-7">
+							<input type ="text" class="form-control" id="txtdeadline" name="txtdeadline" placeholder="DD-MM-YYYY H:M:S" />
+						</div >
+
+						<div style="padding-left: 40px" class  ="col-sm-12"><span id="helpBlock2" class="help-block" >Vui lòng nhập theo mẫu DD-MM/-YYY H:M:S </span></div>
+					</div>
+
+					@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+					@endif
 
 
 
-@endsection
+
+
+					<div class="modal-footer">
+
+						<input type ="submit" name="submit" id="dangkidetai" class="btn btn-primary btn-lg btn-block" value="Đặt">
+
+					</div>     
+				</form>
+			</div>
+		</div>
+		s
+
+
+
+
+	</div>
+
+	
+
+
+
+	@endsection
 
